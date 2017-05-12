ef64814f6ff74d4433f80a09a87823550098a6a8
allimiko <benq-96@mail.ru>
2017-05-12 23:49:47 +0300

    todo

--- src/Lesson4/Task6.java
+++ src/Lesson4/Task6.java
@@ -4,16 +4,4 @@ package Lesson4;
  * Created by Monsters on 18.04.2017.
  */
 public class Task6 {
-    dfgsdfgdf
-    dfg
-            df
-    gdf
-                    sdcasdasd
-    sad
-                            as
-    das
-                                    das
-    d
-                                            as
-    d
 }
